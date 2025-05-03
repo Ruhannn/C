cd9162b6a7b84aaf77f289636742031c3c699a51
Ruhannn <ruhanrouf17@gmail.com>
2025-05-03 19:57:21 +0600

    added types comments

--- types.c
+++ types.c
@@ -1,19 +1,40 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-int main(){
-int age = 18;
-int year = 2006;
-float gpa = 4.0;
+int main()
+{
+    // ? whole number (4 bytes in morder system)
+    int age = 18;
+    int year = 2006;
+    float gpa = 4.0;
 
-char grade = 'A';
-char name[] = "ruhan";
+    // ? single character (1 bytes)
+    char grade = 'A';
+    //  ['r', 'u', 'h', 'a', 'n', '\0']
+    /*
+    ? array of character
+    ! (length + 1[for \0 bytes]) for example here is (5+1)=6 bytes
+    * ['r', 'u', 'h', 'a', 'n', '\0']
+    */
+    char name[] = "ruhan";
 
-double PI = 3.141592653589793;
+    // ? double-precision decimal number (8 bytes :0)
+    double PI = 3.141592653589793;
 
-printf("you are %d years old (%d)\n", age, year);
-printf("your gpa is %.0f (%c)\n", gpa, grade);
-printf("you are %s\n", name);
-printf("the value of PI is %.15lf", PI);
+    // ? true oe false , 1 or 0 (1 bytes, requires <stdbool.h>)
+    bool isMarried = false;
+
+    printf("you are %d years old (%d)\n", age, year);
+    printf("your gpa is %.0f (%c)\n", gpa, grade);
+
+    printf("you are %s\n", name);
+    printf("the value of PI is %.15lf\n", PI);
+
+    if (isMarried) {
+        printf("woah be happy <3");
+    } else {
+        printf("ummmmmm 0_0");
+    }
 
     return 0;
 }
